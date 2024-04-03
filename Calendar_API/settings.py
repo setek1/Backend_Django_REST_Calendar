cf217608ab8dba08c25a2248351c0e980efb173f
Sebastian Esquivel <set1.esq@gmail.com>
2024-04-03 18:43:28 -0300

    Instalacion de corsheaders y conexion con front end

--- Calendar_API/settings.py
+++ Calendar_API/settings.py
@@ -39,11 +39,13 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'calendarDates',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -123,3 +125,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CORS_ORIGIN_ALLOW_ALL = True
+# Permitimos que las solicitudes incluyan credenciales de autenticacion como cookies ** INVESTIGAR PUEDE SER PELIGROSO
+CARS_ALLOW_CREDENTIALS = True
